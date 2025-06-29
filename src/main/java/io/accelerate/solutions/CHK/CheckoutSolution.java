680314e0cb910fd78869112ee4fdf56904bed26a
sbx_user1051 <sbx_user1051@169.254.12.113>
2025-06-29 19:00:53 +0000

    Sun Jun 29 19:00:53 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -7,18 +7,28 @@ import java.util.HashMap;
 public class CheckoutSolution {
     public Integer checkout(String skus) {
         HashMap<Character, Integer> skuToCount = new HashMap<>();
-        for (int i = 0; i<skus.length(); i++) {
+        skuToCount.put('A', 0);
+        skuToCount.put('B', 0);
+        skuToCount.put('C', 0);
+        skuToCount.put('D', 0);
+        for (int i = 0; i < skus.length(); i++) {
             char c = skus.charAt(i);
             if (c == 'A') {
                 int currentCount = skuToCount.get('A');
                 skuToCount.put('A', ++currentCount);
 //                skuToCount.compute('A', (k, currentCount) -> currentCount);
             } else if (c=='B') {
-                skuToCount.compute('B', (k, currentCount) -> currentCount);
+                int currentCount = skuToCount.get('B');
+                skuToCount.put('B', ++currentCount);
+//                skuToCount.compute('B', (k, currentCount) -> currentCount);
             } else if (c=='C') {
-                skuToCount.compute('C', (k, currentCount) -> currentCount);
+                int currentCount = skuToCount.get('C');
+                skuToCount.put('C', ++currentCount);
+//                skuToCount.compute('C', (k, currentCount) -> currentCount);
             } else if (c=='D') {
-                skuToCount.compute('D', (k, currentCount) -> currentCount);
+                int currentCount = skuToCount.get('D');
+                skuToCount.put('D', ++currentCount);
+//                skuToCount.compute('D', (k, currentCount) -> currentCount);
             } else return -1;
         }
         int skuAOfferEligibleCount = skuToCount.get('A')/3;
@@ -35,3 +45,4 @@ public class CheckoutSolution {
         return totalA + totalB + totalC + totalD;
     }
 }
+
