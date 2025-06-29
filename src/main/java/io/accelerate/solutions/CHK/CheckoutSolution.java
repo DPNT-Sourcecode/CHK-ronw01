ac191d4e6cff61b90c5451c37add0a613c4eab67
sbx_user1051 <sbx_user1051@169.254.12.113>
2025-06-29 18:54:53 +0000

    Sun Jun 29 18:54:53 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -2,8 +2,35 @@ package io.accelerate.solutions.CHK;
 
 import io.accelerate.runner.SolutionNotImplementedException;
 
+import java.util.HashMap;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        HashMap<Character, Integer> skuToCount = new HashMap<>();
+        for (int i = 0; i<skus.length(); i++) {
+            char c = skus.charAt(i);
+            if (c == 'A') {
+                skuToCount.compute('A', (k, currentCount) -> currentCount);
+            } else if (c=='B') {
+                skuToCount.compute('B', (k, currentCount) -> currentCount);
+            } else if (c=='C') {
+                skuToCount.compute('C', (k, currentCount) -> currentCount);
+            } else if (c=='D') {
+                skuToCount.compute('D', (k, currentCount) -> currentCount);
+            } else return -1;
+        }
+        int skuAOfferEligibleCount = skuToCount.get('A')/3;
+        int skuAOfferNonEligibleCount = skuToCount.get('A')%3;
+        int totalA = skuAOfferEligibleCount * 130 + skuAOfferNonEligibleCount * 50;
+
+        int skuBOfferEligibleCount = skuToCount.get('B')/2;
+        int skuBOfferNonEligibleCount = skuToCount.get('B')%2;
+        int totalB = skuBOfferEligibleCount * 45 + skuBOfferNonEligibleCount * 30;
+
+        int totalC = skuToCount.get('C') * 20;
+        int totalD = skuToCount.get('D') * 15;
+
+        return totalA + totalB + totalC + totalD;
     }
 }
+
--- src/test/java/io/accelerate/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/io/accelerate/solutions/CHK/CheckoutSolutionTest.java
@@ -1,4 +1,23 @@
 package io.accelerate.solutions.CHK;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class CheckoutSolutionTest {
+    private CheckoutSolution checkoutSolution;
+
+    @BeforeEach
+    public void setup() {
+        checkoutSolution = new CheckoutSolution();
+    }
+
+    @Test
+    public void checkout_test() {
+        // 3 As for 130 + 2 Bs for 45 + 1 C + 1 D = 210
+        assertThat(checkoutSolution.checkout("AAABBCD"), equalTo(210));
+    }
 }
+
