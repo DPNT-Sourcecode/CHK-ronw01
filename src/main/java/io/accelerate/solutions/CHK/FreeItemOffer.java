15e9effe2cf36e802d79fe105e2492d0166dae0e
sbx_user1051 <sbx_user1051@169.254.95.125>
2025-07-02 00:58:14 +0000

    Wed Jul 02 00:58:14 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/FreeItemOffer.java
+++ src/main/java/io/accelerate/solutions/CHK/FreeItemOffer.java
@@ -14,22 +14,30 @@ public class FreeItemOffer implements Offer{
 
     @Override
     public BigDecimal apply(Map<SKU, Integer> basket, SKU sku) {
+        // when free item is same as required item
         if (sku.getName() == freeItem) {
-            int count = 
-        }
-        int totalBundleCount = basket.get(sku)/eligibilityCount;
-        // we need to find the existing count of free item in the basket and decrement it by bundle count
-        for (Map.Entry<SKU, Integer> entry : basket.entrySet()) {
-            if (entry.getKey().getName() == this.freeItem) {
-                int freeItemCountInBasket = entry.getValue() - totalBundleCount;
-                // make sure we do not extract more than existing quantity of free item in the basket
-                if(freeItemCountInBasket < 0) {
-                    freeItemCountInBasket = 0;
+            int count = basket.get(sku);
+            if (count/(eligibilityCount+1) >= 1) {
+                count -= count/(eligibilityCount+1);
+                basket.put(sku, count);
+            }
+            // when free item is different to required item
+        } else {
+            int totalBundleCount = basket.get(sku)/eligibilityCount;
+            // we need to find the existing count of free item in the basket and decrement it by bundle count
+            for (Map.Entry<SKU, Integer> entry : basket.entrySet()) {
+                if (entry.getKey().getName() == this.freeItem) {
+                    int freeItemCountInBasket = entry.getValue() - totalBundleCount;
+                    // make sure we do not extract more than existing quantity of free item in the basket
+                    if(freeItemCountInBasket < 0) {
+                        freeItemCountInBasket = 0;
+                    }
+                    entry.setValue(freeItemCountInBasket);
+                    break;
                 }
-                entry.setValue(freeItemCountInBasket);
-                break;
             }
         }
+
         return BigDecimal.ZERO; // free item offer just decrements eligible items from basket
     }
 
@@ -38,4 +46,5 @@ public class FreeItemOffer implements Offer{
         return eligibilityCount;
     }
 }
+
 
